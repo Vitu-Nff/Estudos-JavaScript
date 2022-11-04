920755ce7c7ea5d4014d62560ba7026735b9394d
Victor Ferreira <vitu.nff@gmail.com>
2022-11-04 16:37:08 -0300

    Tailwind CSS Installed

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+
+@section('content')
+    <p> Hello, <span>World!</span> </p>
+@endsection
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,7 +5,11 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    @stack('styles')
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+    @stack('scripts')
     <script src="{{ mix('js/app.js') }}" defer></script>
 
     <title> @yield('title') - JavaScript</title>
@@ -20,7 +24,5 @@
 
 @section('footer')
 @show
-
-@stack('scripts')
 </body>
 </html>
